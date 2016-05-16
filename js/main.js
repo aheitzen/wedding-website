197d6a28ce34ad332cc3e5ab0a5b5b892ea036e4
aheitzen <alexaheitzenrader@yahoo.com>
2016-05-16 15:20:48 -0700

    added fixed positioning

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function (){
-            $("#activearrow").click(function (){
+    $("#activearrow").click(function (){
+            	console.log("I work");
                 $('html, body').animate({
                     scrollTop: $("#pictures").offset().top
                 }, 1000);
             });
         });
+
+$(document).ready(function() {
+		$(".fancybox").fancybox();
+	});
